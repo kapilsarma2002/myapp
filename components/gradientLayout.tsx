bd3ec2e092941e25ba0db954485b93562c0208ec
kapilsarma2002 <sarmakapil1818@gmail.com>
2022-07-26 16:24:57 +0530

    profile

--- components/gradientLayout.tsx
+++ components/gradientLayout.tsx
@@ -15,8 +15,27 @@ const GradientLayout = ({
   roundImage
 }) => {
   return (
-    <Box height='100vh' overflowY='auto' bgGradient={`linear(${color}.500 0)`}>
-
+    <Box 
+      height='calc(100vh - 100px)'
+      overflowY='auto'
+      bgGradient={`linear(${color}.500 0%, ${color}.600 15%, ${color}.700 40%, rgba(0,0,0,0.95) 75%)`}
+    >    
+        <Flex bg={`${color}.600`} padding='40px' align='end'>
+          <Box padding='20px'>
+            <Image boxSize='160px' boxShadow='5xl' src={image} borderRadius={roundImage ? '100%' : '3px'} />
+          </Box>
+          <Box padding='20px' lineHeight='60px' color='white'>
+            <Text fontSize='x-small' fontWeight='bold' casing='uppercase'>
+              {subtitle}
+            </Text>
+            <Text fontSize='6xl'>
+              {title}
+            </Text>
+            <Text fontSize='x-small' fontWeight='100'>
+              {description}
+            </Text>
+          </Box>
+        </Flex>
     </Box>
   )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,20 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import GradientLayout from '../components/gradientLayout'
 import styles from '../styles/Home.module.css'
 
 const Home = () => {
   return (
-    <div>
-      Home
-    </div>
+    <GradientLayout
+      color='red'
+      subtitle='profile'
+      title='Kapil'
+      description='15 public playlists'
+      image='kapil.jpeg'
+      roundImage
+    >
+      <div>Home page</div>
+    </GradientLayout>
   )
 }
 
