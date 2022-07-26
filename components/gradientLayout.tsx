bb7f044cb709877871c8be754316cfdad030ec9b
kapilsarma2002 <sarmakapil1818@gmail.com>
2022-07-26 19:47:40 +0530

    artists ui

--- components/gradientLayout.tsx
+++ components/gradientLayout.tsx
@@ -20,22 +20,24 @@ const GradientLayout = ({
       overflowY='auto'
       bgGradient={`linear(${color}.500 0%, ${color}.600 15%, ${color}.700 40%, rgba(0,0,0,0.95) 75%)`}
     >    
-        <Flex bg={`${color}.600`} padding='40px' align='end'>
-          <Box padding='20px'>
-            <Image boxSize='160px' boxShadow='5xl' src={image} borderRadius={roundImage ? '100%' : '3px'} />
-          </Box>
-          <Box padding='20px' lineHeight='60px' color='white'>
-            <Text fontSize='x-small' fontWeight='bold' casing='uppercase'>
-              {subtitle}
-            </Text>
-            <Text fontSize='6xl'>
-              {title}
-            </Text>
-            <Text fontSize='x-small' fontWeight='100'>
-              {description}
-            </Text>
-          </Box>
-        </Flex>
+      <Flex bg={`${color}.600`} padding='40px' align='end'>
+        <Box padding='20px'>
+          <Image boxSize='160px' boxShadow='5xl' src={image} borderRadius={roundImage ? '100%' : '3px'} />
+        </Box>
+        <Box padding='20px' lineHeight='60px' color='white'>
+          <Text fontSize='sm' fontWeight='bold' casing='uppercase'>
+            {subtitle}
+          </Text>
+          <Text fontSize='6xl'>
+            {title}
+          </Text>
+          <Text fontSize='sm' fontWeight='100'>
+            {description}
+          </Text>
+        </Box>
+      </Flex>
+
+      <Box paddingY="50px">{children}</Box>
     </Box>
   )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,55 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import GradientLayout from '../components/gradientLayout'
+import prisma from '../lib/prisma'
 import styles from '../styles/Home.module.css'
+import {Box, Text, Flex} from '@chakra-ui/layout'
+import { Image } from '@chakra-ui/react'
 
-const Home = () => {
+const Home = ({artists}) => {
+  // console.log(artists)
   return (
     <GradientLayout
-      color='red'
+      color='purple'
       subtitle='profile'
-      title='Kapil'
+      title='Kapil Sarma'
       description='15 public playlists'
       image='kapil.jpeg'
       roundImage
     >
-      <div>Home page</div>
+      <Box color='white' paddingX='40px'>
+        <Box marginBottom='40px'>
+          <Text fontSize='2xl' fontWeight='bold'>Top artists this month</Text>
+          <Text fontSize='medium'>Only visible to you</Text>
+        </Box>
+        <Flex>
+          {
+            artists.map((artist) => (
+              <Box paddingX='10px' width='20%'>
+                <Box bg='gray.900' borderRadius='8px' padding='15px' width='100%'>
+                  <Image
+                    src='https://2.bp.blogspot.com/_zF_VEO13V7k/TRHLbctPQFI/AAAAAAAABMs/YV8RhPb9UuA/s1600/little_krishna.jpg'
+                    borderRadius='100%'
+                  />
+                  <Box marginTop='20px'>
+                    <Text fontSize='large'>{artist.name}</Text>
+                    <Text fontSize='x-small'>Artist</Text>
+                  </Box>
+                </Box>
+              </Box>
+            ))
+          }
+        </Flex>
+      </Box>
     </GradientLayout>
   )
+}
+
+export const getServerSideProps = async () => {
+  const artists = await prisma.artist.findMany({})
+
+  return {
+    props: {artists}
+  }
 }
 
 export default Home
